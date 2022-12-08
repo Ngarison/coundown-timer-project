f35a91e4f2d05ebced051ddf223befa1a56a511f
Ngarison <jules.ngari@gmail.com>
2022-12-08 09:57:03 +0100

    Création of a countdown timer for fun

---
+++ script.js
@@ -0,0 +1,42 @@
+//Récupération des éléments par leurs identifiants html
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+
+
+const newYears='1 Jan 2023';
+//Création de la fonction 
+function countdown(){
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+
+    const mins = Math.floor(totalSeconds / 60) % 60 ;
+
+    const seconds = Math.floor(totalSeconds) % 60; 
+
+    //console.log(days, hours, mins;  )
+
+    //Affichage de éléments sur la page HTML
+    daysEl.innerHTML= days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML =  formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+
+}
+
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
+}
+
+//initial call
+countdown();
+
+setInterval(countdown, 1000);
